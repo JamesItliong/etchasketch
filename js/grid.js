587328029438daf1bb2f3da5ad1e889b31bf3a82
James Itliong <kenneth.cloud15@gmail.com>
2016-12-24 18:58:47 +0800

    added monochrome color feature

--- js/grid.js
+++ js/grid.js
@@ -1,17 +1,23 @@
 var style = 0;
+var r, g, b;
+var h1, h2, h3;
 
 $(document).ready(function() {
-	// CREATE AN INITIAL 60X60 GRID
 	createGrid(50*50, 700/50);
+	draw();
+});
 
+function draw() {
 	$(".square").hover(function() {
-		if(style === 0) {
+		if(style === 0) { 		 //DEFAULT COLOR
 			$(this).css("background-color", "black");
-		} else {
+		} else if(style === 1) { //MONOCHROME COLOR
+			$(this).css("background-color", monochromeColor($(this).css("background-color")));
+		} else if(style === 2) { //RANDOM COLOR
 			$(this).css("background-color", generateRandomColor(50));
 		}
 	});
-});
+}
 
 function resizeGrid() {
 	deleteGrid();
@@ -21,6 +27,7 @@ function resizeGrid() {
 	if(nSquares != null && nSquares <= 100) {
 		var squareSize = (700 / nSquares);
 		createGrid(nSquares*nSquares, squareSize);
+		draw();
 	} else {
 		alert(":O Please enter a valid number. BTW it's between 1 and 100.");
 	}
@@ -30,7 +37,7 @@ function createGrid(number, size) {
 	var $square;
 
 	for(var i = 0; i < number; i++) {
-		$square = $("<div></div>");
+		$square = $("<div id=" + "'" + i + "'" + "></div>");
 		$square.addClass('square');
 		$square.css({'height': size+"px", 'width': size+"px"});
 		$(".gridbox").append($square);
@@ -42,34 +49,65 @@ function deleteGrid() {
 }
 
 function clearGrid() {
-	$(".square").css("background-color", "gray");
+	$(".square").css("background-color", "white");
 }
 
-//Function adapted from http://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/14187677#14187677
+//Function from http://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript/14187677#14187677
 function generateRandomColor(brightness) {
 	function generateRandomHexPair() {
 		var r = 255 - brightness; //?
 		var n = 0 | ((Math.random() * r) + brightness); //?
 		var s = n.toString(16); //convert number to a HEX string
+
 		return (s.length == 1) ? '0'+s : s;
 	}
 	return '#' + generateRandomHexPair() + generateRandomHexPair() + generateRandomHexPair();
 }
 
+/**
+	Use variable.toString(16) to convert decimal to hex
+	Use parseInt(hexValue, 16) to convert hex to decimal
+	Use .css("attribute") to obtain value
+	format: rgb(0, 0, 0)
+*/
+
+
+function monochromeColor(prevColor) {
+	var brightness = 15;
+	var values = prevColor.split(/[\D]+/);
+
+	r = parseInt(values[1], 10);
+	g = parseInt(values[2], 10);
+	b = parseInt(values[3], 10);
+
+	r = r > brightness ? (r - brightness) : 0;
+	g = g > brightness ? (g - brightness) : 0;
+	b = b > brightness ? (b - brightness) : 0;
+
+	h1 = r.toString(16);
+	h2 = g.toString(16);
+	h3 = b.toString(16);
+
+	h1 = (h1.length === 1) ? '0'+h1 : h1;
+	h2 = (h2.length === 1) ? '0'+h2 : h2;
+	h3 = (h3.length === 1) ? '0'+h3 : h3;
+
+	var newColor = "#" + h1 + h2 + h3;
+	return newColor;
+}
+
+
 function setStyle(option) {
 	style = option;
 }
 
-
-
-/**
-	Features to add:
-	1. Press a button to resize the screen (DONE)
-	2. Press a button to clear the screen (DONE)
-	3. Buttons for:
-		- random colors
-		- increasing darkness (monochrome)
-		- WITHOUT CHANGING THE SCREEN
-
-
-*/
+function captureDrawing() {
+	html2canvas($('.gridbox'), {
+		onrendered: function(canvas) {
+			var url = canvas.toDataURL();
+			var downloadDrawing = $("<a>").attr("href", url).attr("download", "sketch.png").appendTo("body");
+			downloadDrawing[0].click();
+            downloadDrawing.remove();
+		}
+	})
+}
